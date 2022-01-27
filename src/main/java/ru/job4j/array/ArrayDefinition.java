bedeb55bcd904ed23b82f340e20a778130e73460
Vitaliy123 <cpc1251@mail.ru>
2022-01-27 14:16:10 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,5 +5,8 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] names = new String[100500];
         float[] prices = new  float[40];
+        System.out.println("Размер массива ages равен: " + ages.length);
+        System.out.println("Размер массива names равен: " + names.length);
+        System.out.println("Размер массива prices равен: " + prices.length);
     }
 }
