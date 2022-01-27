f0bad6a69e43d764348115a92099be31c963fe2e
Vitaliy123 <cpc1251@mail.ru>
2022-01-27 17:20:11 +0300

    6.3. Массив заполнен true или false [#53857]

--- src/test/java/ru/job4j/array/CheckTest.java
+++ src/test/java/ru/job4j/array/CheckTest.java
@@ -3,23 +3,32 @@ package ru.job4j.array;
 import org.junit.Assert;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
-
 public class CheckTest {
-
     @Test
-    public void whenAllisTrue() {
-        boolean[] in = new boolean[]{true, true, true};
-        boolean expected = true;
-        boolean out = Check.mono(in);
-        Assert.assertEquals(out, expected);
+    public void whenDataMonoByTrueThenTrue() {
+        boolean[] data = new boolean[] {true, true, true};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
     }
 
     @Test
-    public void whenNotAllisTrue() {
-        boolean[] in = new boolean[]{true, false, true};
-        boolean expected = false;
-        boolean out = Check.mono(in);
-        Assert.assertEquals(out, expected);
+    public void whenDataNotMonoByTrueThenFalse() {
+        boolean[] data = new boolean[] {true, false, true};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
+    }
+
+    @Test
+    public void whenDataMonoByFalseThenTrue() {
+        boolean[] data = new boolean[] {false, false, false};
+        boolean result = Check.mono(data);
+        Assert.assertTrue(result);
+    }
+
+    @Test
+    public void whenDataNotMonoByFalseThenFalse() {
+        boolean[] data = new boolean[] {false, true, false};
+        boolean result = Check.mono(data);
+        Assert.assertFalse(result);
     }
 }
