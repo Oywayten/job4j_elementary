656a47faa85f0eb0efc89dfc26d0e41651a45ee9
Vitaliy123 <cpc1251@mail.ru>
2022-08-03 21:25:19 +0300

    job4j-31 6.9. String - это массив [#504884]
    Написал метод для валидации имени переменной. Работает на вспомогательных методах.

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,44 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    private static final int SPECIAL_BEGIN = 36;
+    private static final int SPECIAL_END = 95;
+    private static final int UPPER_BEGIN = 65;
+    private static final int UPPER_END = 90;
+    private static final int LOWER_BEGIN = 97;
+    private static final int LOWER_END = 122;
+    private static final int FIRST_CHAR = 0;
+    private static final int START_INT = 1;
+
+    public static boolean isNameValid(String name) {
+
+        boolean isValid = false;
+        if (!name.isEmpty() && isLowerLatinLetter(name.codePointAt(FIRST_CHAR))) {
+            for (int i = START_INT; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isLowerLatinLetter(code)
+                        || isUpperLatinLetter(code)
+                        || isSpecialSymbol(code)
+                        || Character.isDigit(name.charAt(i))) {
+                    isValid = true;
+                } else {
+                    isValid = false;
+                    break;
+                }
+            }
+        }
+        return isValid;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == SPECIAL_BEGIN || code == SPECIAL_END;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= UPPER_BEGIN && code <= UPPER_END;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= LOWER_BEGIN && code <= LOWER_END;
+    }
+}
